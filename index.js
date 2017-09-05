21d07ac9311392c4b9827355733ba74101e0aef7
Zoe Bryant <zoeabryant@gmail.com>
2017-09-05 11:35:56 +0100

    Tidying up

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 class AddressFinder extends React.Component {
   constructor() {
     super();
@@ -10,27 +9,29 @@ class AddressFinder extends React.Component {
     };
   }
 
-  clearSearchValue = () => this.setState({
-    results: [],
-    searchTerm: '',
-  });
+  clearSearchValue = () =>
+    this.setState({
+      results: [],
+      searchTerm: '',
+    });
 
-  pickAddress = (address) => this.setState({
-    chosenAddress: address,
-    results: [],
-    searchTerm: '',
-  });
+  pickAddress = address =>
+    this.setState({
+      chosenAddress: address,
+      results: [],
+      searchTerm: '',
+    });
 
   handleChange = e => {
     this.setState({
-      searchTerm: e.target.value
+      searchTerm: e.target.value,
     });
 
-    if(e.target.value === 'error') {
+    if (e.target.value === "error") {
       return this.setState({
         results: [],
         error: true,
-      })
+      });
     }
     return this.setState({
       results: [
@@ -41,16 +42,11 @@ class AddressFinder extends React.Component {
         '30 Briggs Lane',
       ],
       error: false,
-    })
+    });
   };
 
   render() {
-    const {
-      chosenAddress,
-      results,
-      error,
-      searchTerm,
-    } = this.state;
+    const { chosenAddress, results, error, searchTerm } = this.state;
     return (
       <div>
         <div>Address finder:</div>
@@ -60,43 +56,33 @@ class AddressFinder extends React.Component {
             value={searchTerm}
             onChange={this.handleChange}
           />
-          {
-            searchTerm.length !== 0 &&
-              <div className="clear_button" onClick={() => this.clearSearchValue()}>
-                <img src="clear.svg" />
-              </div>
-          }
+          {searchTerm.length !== 0 && (
+            <div
+              className="clear_button"
+              onClick={() => this.clearSearchValue()}
+            >
+              <img src="clear.svg" />
+            </div>
+          )}
         </div>
-        {
-          error &&
-            <div className="error">Something is wrong!</div>
-        }
-        {
-          results.length > 0 &&
-            <div className="results">
-              {results.map(result =>
-                <div
-                  onClick={() => this.pickAddress(result)}
-                  className="a_result"
-                  key={result}
-                >
-                  {result}
-                </div>
-              )}
-            </div>
-        }
-        {
-          chosenAddress &&
-            <div className="chosen_address">
-              {chosenAddress}
-            </div>
-        }
+        {error && <div className="error">Something is wrong!</div>}
+        {results.length > 0 && (
+          <div className="results">
+            {results.map(result => (
+              <div
+                onClick={() => this.pickAddress(result)}
+                className="a_result"
+                key={result}
+              >
+                {result}
+              </div>
+            ))}
+          </div>
+        )}
+        {chosenAddress && <div className="chosen_address">{chosenAddress}</div>}
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(
-  <AddressFinder />,
-  document.getElementById('root')
-);
+ReactDOM.render(<AddressFinder />, document.getElementById("root"));
